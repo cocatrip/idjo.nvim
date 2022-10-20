9c8d8c19a811580c8ae0f11cc8420d00195ffe6a
Vian <cocatrip@proton.me>
2022-10-20 12:05:38 +0700

    a

--- lua/lualine/themes/idjo.lua
+++ lua/lualine/themes/idjo.lua
@@ -8,7 +8,7 @@ local idjo_lualine = {
         c = {fg = c.grey, bg = c.bg_d},
     },
     normal = {
-        a = {fg = c.bg, bg = c.bg1, gui = 'bold'},
+        a = {fg = c.fg, bg = c.bg1, gui = 'bold'},
         b = {fg = c.fg, bg = c.bg3},
         c = {fg = c.fg, bg = c.bg_d},
     },
