9ba6fc2c585009d10b21afb998da936f502ca342
Vian <cocatrip@proton.me>
2022-10-20 12:05:38 +0700

    Modify lualine

--- lua/lualine/themes/idjo.lua
+++ lua/lualine/themes/idjo.lua
@@ -6,6 +6,9 @@ local idjo_lualine = {
         a = {fg = c.grey, bg = c.bg_d, gui = 'bold'},
         b = {fg = c.grey, bg = c.bg_d},
         c = {fg = c.grey, bg = c.bg_d},
+        x = {fg = c.grey, bg = c.bg_d},
+        y = {fg = c.grey, bg = c.bg_d},
+        z = {fg = c.grey, bg = c.bg_d, gui = 'bold'},
     },
     normal = {
         a = {fg = c.green, bg = c.bg1, gui = 'bold'},
@@ -17,15 +20,35 @@ local idjo_lualine = {
     },
     visual  = {
         a = {fg = c.purple   , bg = c.bg1, gui = 'bold'}
+        b = {fg = c.fg, bg = c.bg3},
+        c = {fg = c.fg, bg = c.bg_d},
+        x = {fg = c.fg, bg = c.bg_d},
+        y = {fg = c.fg, bg = c.bg3},
+        z = {fg = c.fg, bg = c.bg1, gui = 'bold'},
     },
     replace = {
         a = {fg = c.red      , bg = c.bg1, gui = 'bold'}
+        b = {fg = c.fg, bg = c.bg3},
+        c = {fg = c.fg, bg = c.bg_d},
+        x = {fg = c.fg, bg = c.bg_d},
+        y = {fg = c.fg, bg = c.bg3},
+        z = {fg = c.fg, bg = c.bg1, gui = 'bold'},
     },
     insert  = {
         a = {fg = c.blue     , bg = c.bg1, gui = 'bold'}
+        b = {fg = c.fg, bg = c.bg3},
+        c = {fg = c.fg, bg = c.bg_d},
+        x = {fg = c.fg, bg = c.bg_d},
+        y = {fg = c.fg, bg = c.bg3},
+        z = {fg = c.fg, bg = c.bg1, gui = 'bold'},
     },
     command = {
         a = {fg = c.yellow   , bg = c.bg1, gui = 'bold'}
+        b = {fg = c.fg, bg = c.bg3},
+        c = {fg = c.fg, bg = c.bg_d},
+        x = {fg = c.fg, bg = c.bg_d},
+        y = {fg = c.fg, bg = c.bg3},
+        z = {fg = c.fg, bg = c.bg1, gui = 'bold'},
     }
 }
 
